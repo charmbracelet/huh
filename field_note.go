fb7e3beeb140abfb95981fc95cade36412eca69d
Christian Rocha <christian@rocha.is>
2023-10-23 14:17:40 -0400

    feat(theme): apply theme to notes

--- field_note.go
+++ field_note.go
@@ -6,36 +6,7 @@ import (
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/glamour"
-	"github.com/charmbracelet/lipgloss"
 )
-
-// NoteStyle is the style of the Note field.
-type NoteStyle struct {
-	Base        lipgloss.Style
-	Title       lipgloss.Style
-	Description lipgloss.Style
-	Next        lipgloss.Style
-	Body        lipgloss.Style
-}
-
-// DefaultNoteStyles returns the default focused style of the Note field.
-func DefaultNoteStyles() (NoteStyle, NoteStyle) {
-	focused := NoteStyle{
-		Base:        lipgloss.NewStyle().Border(lipgloss.ThickBorder(), false).Margin(1, 0).BorderForeground(lipgloss.Color("8")),
-		Title:       lipgloss.NewStyle().Foreground(lipgloss.Color("3")).Margin(1),
-		Description: lipgloss.NewStyle().Foreground(lipgloss.Color("8")),
-		Body:        lipgloss.NewStyle().Foreground(lipgloss.Color("15")).Margin(1),
-		Next:        lipgloss.NewStyle().Background(lipgloss.Color("5")).Foreground(lipgloss.Color("3")).Margin(1, 2).Padding(0, 1).Bold(true),
-	}
-	blurred := NoteStyle{
-		Base:        lipgloss.NewStyle().Border(lipgloss.HiddenBorder(), false).BorderLeft(true).MarginBottom(1),
-		Title:       lipgloss.NewStyle().Foreground(lipgloss.Color("8")).Margin(1),
-		Description: lipgloss.NewStyle().Foreground(lipgloss.Color("8")),
-		Body:        lipgloss.NewStyle().Foreground(lipgloss.Color("8")).Margin(1),
-		Next:        lipgloss.NewStyle().Foreground(lipgloss.Color("8")).Margin(0, 2).MarginBottom(1),
-	}
-	return focused, blurred
-}
 
 // Note is a form note field.
 type Note struct {
@@ -43,20 +14,14 @@ type Note struct {
 	description string
 
 	showNextButton bool
-	style          *NoteStyle
-	blurredStyle   NoteStyle
-	focusedStyle   NoteStyle
+	focused        bool
 	theme          *Theme
 }
 
 // NewNote creates a new note field.
 func NewNote() *Note {
-	focused, blurred := DefaultNoteStyles()
 	return &Note{
 		showNextButton: false,
-		style:          &blurred,
-		focusedStyle:   focused,
-		blurredStyle:   blurred,
 	}
 }
 
@@ -78,22 +43,15 @@ func (n *Note) Next(show bool) *Note {
 	return n
 }
 
-// Styles sets the styles of the note field.
-func (n *Note) Styles(focused, blurred NoteStyle) *Note {
-	n.blurredStyle = blurred
-	n.focusedStyle = focused
-	return n
-}
-
 // Focus focuses the note field.
 func (n *Note) Focus() tea.Cmd {
-	n.style = &n.focusedStyle
+	n.focused = true
 	return nil
 }
 
 // Blur blurs the note field.
 func (n *Note) Blur() tea.Cmd {
-	n.style = &n.blurredStyle
+	n.focused = false
 	return nil
 }
 
@@ -117,9 +75,15 @@ func (n *Note) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 // View renders the note field.
 func (n *Note) View() string {
-	var sb strings.Builder
+	styles := n.theme.Blurred
+	if n.focused {
+		styles = n.theme.Focused
+	}
 
-	var body string
+	var (
+		sb   strings.Builder
+		body string
+	)
 
 	if n.title != "" {
 		body = fmt.Sprintf("# %s\n", n.title)
@@ -130,9 +94,9 @@ func (n *Note) View() string {
 	md, _ := glamour.Render(body, "auto")
 	sb.WriteString(md)
 	if n.showNextButton {
-		sb.WriteString(n.style.Next.Render("Next"))
+		sb.WriteString(styles.Next.Render("Next"))
 	}
-	return n.style.Base.Render(strings.TrimSpace(sb.String()))
+	return styles.Base.Render(strings.TrimSpace(sb.String()))
 }
 
 // Run runs an accessible note field.
