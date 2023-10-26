1bff016cc4ec690af998db998833c4d58c1c5f55
Maas Lalani <maas@lalani.dev>
2023-10-26 14:50:22 -0400

    feat: implement filtering

--- field_select.go
+++ field_select.go
@@ -16,9 +16,10 @@ type Select[T any] struct {
 	value *T
 
 	// customization
-	title       string
-	description string
-	options     []Option[T]
+	title           string
+	description     string
+	options         []Option[T]
+	filteredOptions []Option[T]
 
 	// error handling
 	validate func(T) error
@@ -48,11 +49,12 @@ func NewSelect[T any](options ...T) *Select[T] {
 	filter.Prompt = "/"
 
 	return &Select[T]{
-		value:     new(T),
-		options:   opts,
-		validate:  func(T) error { return nil },
-		filtering: false,
-		filter:    filter,
+		value:           new(T),
+		options:         opts,
+		filteredOptions: opts,
+		validate:        func(T) error { return nil },
+		filtering:       false,
+		filter:          filter,
 	}
 }
 
@@ -77,6 +79,7 @@ func (s *Select[T]) Description(description string) *Select[T] {
 // Options sets the options of the select field.
 func (s *Select[T]) Options(options ...Option[T]) *Select[T] {
 	s.options = options
+	s.filteredOptions = options
 	return s
 }
 
@@ -116,6 +119,11 @@ func (s *Select[T]) Init() tea.Cmd {
 
 // Update updates the select field.
 func (s *Select[T]) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	if s.filtering {
+		s.filter, cmd = s.filter.Update(msg)
+	}
+
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		s.err = nil
@@ -126,11 +134,22 @@ func (s *Select[T]) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case key.Matches(msg, s.keymap.SetFilter):
 			s.setFilter(false)
 		case key.Matches(msg, s.keymap.Up):
+			// When filtering we should ignore j/k keybindings
+			if s.filtering && msg.String() == "k" {
+				break
+			}
 			s.selected = max(s.selected-1, 0)
 		case key.Matches(msg, s.keymap.Down):
-			s.selected = min(s.selected+1, len(s.options)-1)
+			// When filtering we should ignore j/k keybindings
+			if s.filtering && msg.String() == "j" {
+				break
+			}
+			s.selected = min(s.selected+1, len(s.filteredOptions)-1)
 		case key.Matches(msg, s.keymap.Prev):
-			value := s.options[s.selected].Value
+			if s.selected >= len(s.filteredOptions) {
+				break
+			}
+			value := s.filteredOptions[s.selected].Value
 			s.err = s.validate(value)
 			if s.err != nil {
 				return s, nil
@@ -138,7 +157,10 @@ func (s *Select[T]) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			*s.value = value
 			return s, prevField
 		case key.Matches(msg, s.keymap.Next):
-			value := s.options[s.selected].Value
+			if s.selected >= len(s.filteredOptions) {
+				break
+			}
+			value := s.filteredOptions[s.selected].Value
 			s.setFilter(false)
 			s.err = s.validate(value)
 			if s.err != nil {
@@ -147,12 +169,23 @@ func (s *Select[T]) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			*s.value = value
 			return s, nextField
 		}
+
+		if s.filtering {
+			s.filteredOptions = s.options
+			if s.filter.Value() != "" {
+				s.filteredOptions = nil
+				for _, option := range s.options {
+					if s.filterFunc(option.Key) {
+						s.filteredOptions = append(s.filteredOptions, option)
+					}
+				}
+			}
+			if len(s.filteredOptions) > 0 {
+				s.selected = min(s.selected, len(s.filteredOptions)-1)
+			}
+		}
 	}
 
-	var cmd tea.Cmd
-	if s.filtering {
-		s.filter, cmd = s.filter.Update(msg)
-	}
 	return s, cmd
 }
 
@@ -180,7 +213,7 @@ func (s *Select[T]) View() string {
 	}
 
 	c := styles.SelectSelector.String()
-	for i, option := range s.options {
+	for i, option := range s.filteredOptions {
 		if s.selected == i {
 			sb.WriteString(c + styles.SelectedOption.Render(option.Key))
 		} else {
@@ -190,7 +223,25 @@ func (s *Select[T]) View() string {
 			sb.WriteString("\n")
 		}
 	}
+
+	for i := len(s.filteredOptions); i < len(s.options)-1; i++ {
+		sb.WriteString("\n")
+	}
+
 	return styles.Base.Render(sb.String())
+}
+
+// setFilter sets the filter of the select field.
+func (s *Select[T]) setFilter(filter bool) {
+	s.filtering = filter
+	s.keymap.SetFilter.SetEnabled(filter)
+	s.keymap.Filter.SetEnabled(!filter)
+}
+
+// filterFunc returns true if the option matches the filter.
+func (s *Select[T]) filterFunc(option string) bool {
+	// XXX: remove diacritics or allow customization of filter function.
+	return strings.Contains(strings.ToLower(option), strings.ToLower(s.filter.Value()))
 }
 
 // Run runs the select field.
@@ -227,13 +278,6 @@ func (s *Select[T]) runAccessible() error {
 	}
 
 	return nil
-}
-
-// setFilter sets the filter of the select field.
-func (s *Select[T]) setFilter(filter bool) {
-	s.filtering = filter
-	s.keymap.SetFilter.SetEnabled(filter)
-	s.keymap.Filter.SetEnabled(!filter)
 }
 
 // WithTheme sets the theme of the select field.
