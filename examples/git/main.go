3e97635fef7a48158a940d936791279923101db6
Maas Lalani <maas@lalani.dev>
2023-11-14 11:16:16 -0500

    feat: git conventional commit form example

---
+++ examples/git/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/charmbracelet/huh"
+)
+
+// types is the possible commit types specified by the conventional commit spec.
+var types = []string{"fix", "feat", "docs", "style", "refactor", "test", "chore", "revert"}
+
+// This form is used to write a conventional commit message. It prompts the user
+// to choose the type of commit as specified in the conventional commit spec.
+// And then prompts for the summary and detailed description of the message and
+// uses the values provided as the summary and details of the message.
+func main() {
+	var commit, scope string
+	var summary, description string
+	var confirm bool
+
+	huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[string]().Title("Type").Value(&commit).Options(huh.NewOptions(types...)...),
+			huh.NewInput().Title("Scope").Value(&scope).Placeholder("scope"),
+		),
+		huh.NewGroup(
+			huh.NewInput().Title("Summary").Value(&summary).Placeholder("Summary of changes"),
+			huh.NewText().Title("Description").Value(&description).Placeholder("Detailed description of changes"),
+		),
+		huh.NewGroup(huh.NewConfirm().Title("Commit changes?").Value(&confirm)),
+	).Run()
+}
