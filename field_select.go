47340b543539f18164c297002be9ac182a8cec65
Vítor Mariano <vmatheus.mariano@gmail.com>
2023-12-17 22:01:17 -0300

    fix: prefill select and multiselect with value (#62)
    * fix: prefill select and multiselect with value
    
    * fix: prefill options when value is set first

--- field_select.go
+++ field_select.go
@@ -12,7 +12,7 @@ import (
 )
 
 // Select is a form select field.
-type Select[T any] struct {
+type Select[T comparable] struct {
 	value *T
 	key   string
 
@@ -40,7 +40,7 @@ type Select[T any] struct {
 }
 
 // NewSelect returns a new select field.
-func NewSelect[T any]() *Select[T] {
+func NewSelect[T comparable]() *Select[T] {
 	filter := textinput.New()
 	filter.Prompt = "/"
 
@@ -56,6 +56,12 @@ func NewSelect[T any]() *Select[T] {
 // Value sets the value of the select field.
 func (s *Select[T]) Value(value *T) *Select[T] {
 	s.value = value
+	for i, o := range s.options {
+		if o.Value == *value {
+			s.selected = i
+			break
+		}
+	}
 	return s
 }
 
@@ -86,9 +92,12 @@ func (s *Select[T]) Options(options ...Option[T]) *Select[T] {
 	s.options = options
 	s.filteredOptions = options
 
-	// Set the cursor to the last selected option.
+	// Set the cursor to the existing value or the last selected option.
 	for i, option := range options {
-		if option.selected {
+		if option.Value == *s.value {
+			s.selected = i
+			break
+		} else if option.selected {
 			s.selected = i
 		}
 	}
