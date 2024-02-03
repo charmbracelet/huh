2eba37ef11c2fe2c7d30c8e7f77626d64b3c157d
anirudhaCodes <anirudhaweb3@gmail.com>
2024-02-03 22:22:55 +0530

    Add ValidateMinLength and ValidateMaxLength

--- validate.go
+++ validate.go
@@ -11,8 +11,28 @@ var ErrInputEmpty = fmt.Errorf("input cannot be empty")
 // ValidateNotEmpty checks if the input is not empty.
 func ValidateNotEmpty() func(s string) error {
 	return func(s string) error {
-		if s == "" {
+		if err := ValidateMinLength(1)(s); err != nil {
 			return ErrInputEmpty
+		}
+		return nil
+	}
+}
+
+// ValidateMinLength checks if the length of the input is at least min.
+func ValidateMinLength(min int) func(s string) error {
+	return func(s string) error {
+		if utf8.RuneCountInString(s) < min {
+			return fmt.Errorf("input must be at least %d characters long", min)
+		}
+		return nil
+	}
+}
+
+// ValidateMaxLength checks if the length of the input is at most max.
+func ValidateMaxLength(max int) func(s string) error {
+	return func(s string) error {
+		if utf8.RuneCountInString(s) > max {
+			return fmt.Errorf("input must be at most %d characters long", max)
 		}
 		return nil
 	}
@@ -21,11 +41,10 @@ func ValidateNotEmpty() func(s string) error {
 // ValidateLength checks if the length of the input is within the specified range.
 func ValidateLength(min, max int) func(s string) error {
 	return func(s string) error {
-		length := utf8.RuneCountInString(s)
-		if length < min || length > max {
-			return fmt.Errorf("input must be between %d and %d", min, max)
+		if err := ValidateMinLength(min)(s); err != nil {
+			return err
 		}
-		return nil
+		return ValidateMaxLength(max)(s)
 	}
 }
 
