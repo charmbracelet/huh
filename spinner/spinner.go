adb514f4566460fdda6359e4cf1a63c6c7a82e04
Caleb Stewart <caleb.stewart94@gmail.com>
2024-03-19 14:59:53 -0400

    Added optional printer argument to spinner action

--- spinner/spinner.go
+++ spinner/spinner.go
@@ -21,13 +21,29 @@ import (
 // ⣾  Loading...
 type Spinner struct {
 	spinner    spinner.Model
-	action     func()
+	action     func(Printer)
 	ctx        context.Context
 	accessible bool
 	output     *termenv.Output
 	title      string
 	titleStyle lipgloss.Style
 }
+
+type Printer interface {
+  Println(values ...interface{})
+  Printf(format string, values ...interface{})
+}
+
+type accessiblePrinter struct {}
+
+func (p accessiblePrinter) Println(values ...interface{}) {
+  fmt.Println(values...)
+}
+
+func (p accessiblePrinter) Printf(format string, values ...interface{}) {
+  fmt.Printf(format, values...)
+}
+
 
 type Type spinner.Spinner
 
@@ -60,8 +76,14 @@ func (s *Spinner) Title(title string) *Spinner {
 
 // Action sets the action of the spinner.
 func (s *Spinner) Action(action func()) *Spinner {
-	s.action = action
+	s.action = func(_ Printer) { action() }
 	return s
+}
+
+// ActionWithProgram sets the action of the spinner with a reference to the Bubble Tea program
+func (s *Spinner) ActionWithProgram(action func(Printer)) *Spinner {
+  s.action = action
+  return s
 }
 
 // Context sets the context of the spinner.
@@ -96,7 +118,7 @@ func New() *Spinner {
 	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("#F780E2"))
 
 	return &Spinner{
-		action:     func() { time.Sleep(time.Second) },
+		action:     func(_ Printer) { time.Sleep(time.Second) },
 		spinner:    s,
 		title:      "Loading...",
 		titleStyle: lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "#FFFDF5", Dark: "#FFFDF5"}),
@@ -145,7 +167,7 @@ func (s *Spinner) Run() error {
 
 	if s.ctx == nil {
 		go func() {
-			s.action()
+			s.action(p)
 			p.Quit()
 		}()
 	}
@@ -160,10 +182,8 @@ func (s *Spinner) runAccessible() error {
 	s.output.HideCursor()
 	frame := s.spinner.Style.Render("...")
 	title := s.titleStyle.Render(s.title)
-	fmt.Println(title + frame)
-	s.action()
+  fmt.Println(title + frame)
+	s.action(accessiblePrinter{})
 	s.output.ShowCursor()
-	s.output.CursorBack(len(frame) + len(title))
-
 	return nil
 }
