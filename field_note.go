fd062fd7103ec27aba094fc961deb7214fa2f45e
Andy Bartelsmeier <abartelsmeier@gmail.com>
2024-05-24 19:45:11 +0100

    feat: optional setting of note next label (#225)
    * feat: optional setting of note next label
    
    * review comments
    
    * move nextLabel up with the other customization fields

--- field_note.go
+++ field_note.go
@@ -13,6 +13,7 @@ type Note struct {
 	// customization
 	title       string
 	description string
+	nextLabel   string
 
 	// state
 	showNextButton bool
@@ -32,6 +33,7 @@ func NewNote() *Note {
 	return &Note{
 		showNextButton: false,
 		skip:           true,
+		nextLabel:      "Next",
 	}
 }
 
@@ -50,6 +52,12 @@ func (n *Note) Description(description string) *Note {
 // Next sets whether to show the next button.
 func (n *Note) Next(show bool) *Note {
 	n.showNextButton = show
+	return n
+}
+
+// NextLabel sets the next button label.
+func (n *Note) NextLabel(label string) *Note {
+	n.nextLabel = label
 	return n
 }
 
@@ -131,7 +139,7 @@ func (n *Note) View() string {
 		sb.WriteString(render(n.description))
 	}
 	if n.showNextButton {
-		sb.WriteString(styles.Next.Render("Next"))
+		sb.WriteString(styles.Next.Render(n.nextLabel))
 	}
 	return styles.Card.Render(sb.String())
 }
