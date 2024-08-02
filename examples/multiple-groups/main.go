25d5d6cb9603c273712f64d0d2247a2b6eacacce
bashbunni <bunni@bashbunni.dev>
2024-08-02 15:49:19 -0700

    chore: add example of multiple groups with varying heights

---
+++ examples/multiple-groups/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/huh"
+)
+
+func main() {
+	f := huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[string]().
+				Options(
+					huh.NewOption("A", "a"),
+					huh.NewOption("B", "b"),
+					huh.NewOption("C", "c"),
+					huh.NewOption("D", "d"),
+					huh.NewOption("E", "e"),
+					huh.NewOption("F", "f"),
+					huh.NewOption("G", "g"),
+					huh.NewOption("H", "h"),
+					huh.NewOption("I", "i"),
+					huh.NewOption("J", "j"),
+					huh.NewOption("K", "k").Selected(true),
+					huh.NewOption("L", "l"),
+					huh.NewOption("M", "m"),
+					huh.NewOption("N", "n"),
+					huh.NewOption("O", "o"),
+					huh.NewOption("P", "p"),
+				),
+		).WithHeight(8),
+		huh.NewGroup(
+			huh.NewMultiSelect[string]().
+				Options(
+					huh.NewOption("A", "a"),
+					huh.NewOption("B", "b"),
+					huh.NewOption("C", "c"),
+					huh.NewOption("D", "d"),
+					huh.NewOption("E", "e"),
+					huh.NewOption("F", "f"),
+					huh.NewOption("G", "g"),
+					huh.NewOption("H", "h"),
+					huh.NewOption("I", "i"),
+					huh.NewOption("K", "k").Selected(true),
+					huh.NewOption("L", "l"),
+					huh.NewOption("M", "m"),
+					huh.NewOption("N", "n"),
+					huh.NewOption("O", "o").Selected(true),
+					huh.NewOption("P", "p"),
+				),
+		).WithHeight(3),
+		huh.NewGroup(
+			huh.NewSelect[string]().
+				Options(
+					huh.NewOption("A", "a"),
+					huh.NewOption("B", "b"),
+					huh.NewOption("C", "c"),
+					huh.NewOption("D", "d"),
+					huh.NewOption("E", "e"),
+					huh.NewOption("F", "f"),
+					huh.NewOption("G", "g"),
+					huh.NewOption("H", "h"),
+					huh.NewOption("I", "i"),
+					huh.NewOption("J", "j"),
+					huh.NewOption("K", "k").Selected(true),
+					huh.NewOption("L", "l"),
+					huh.NewOption("M", "m"),
+					huh.NewOption("N", "n"),
+					huh.NewOption("O", "o"),
+					huh.NewOption("P", "p"),
+				),
+		).WithHeight(5),
+	)
+
+	if err := f.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Oof: %v", err)
+	}
+}
